c67b41af5b38c90e26a0a96e6eae575947894adc
Edoardo <edoardo.malfatto@hotmail.it>
2021-04-01 18:45:41 +0200

    bonus 1 v1

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ Vue.config.devtools = true;
 var root = new Vue({
     el: '#app',
     data: {
-        diskBoxes: undefined
+        All: "All",
+        Rock: "Rock",
+        Jazz: "Jazz",
+        Pop: "Pop",
+        Metal: "Metal",
+        diskBoxes: undefined,
+        rockDiskBoxes: [],
+        jazzDiskBoxes: [],
+        metalDiskBoxes: [],
+        popDiskBoxes: [],
+        selectedGenere: "All",
+        aviableGeneres: []
     },
     methods: {
         addMessage: function() {},
@@ -12,10 +23,28 @@ var root = new Vue({
     mounted() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((response) => {
-                console.log(1, this.diskBoxes)
                 this.diskBoxes = response.data.response;
-                console.log(2, this.diskBoxes)
-            });
+                this.diskBoxes.forEach(element => {
+                    if (!this.aviableGeneres.includes(element.genre)) {
+                        this.aviableGeneres.push(element.genre);
+                        console.log(this.aviableGeneres);
+                    }
+                    switch (element.genre) {
+                        case "Rock":
+                            this.rockDiskBoxes.push(element);
+                            break;
+                        case "Jazz":
+                            this.jazzDiskBoxes.push(element);
+                            console.log(this.jazzDiskBoxes);
+                            break;
+                        case "Pop":
+                            this.popDiskBoxes.push(element);
+                            break;
+                        case "Metal":
+                            this.metalDiskBoxes.push(element);
+                            break;
+                    }
+                });
+            })
     }
-
 });
